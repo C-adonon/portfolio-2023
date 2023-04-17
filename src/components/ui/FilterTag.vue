0200ef8d1cbdf1a6ca23da72cbceb018f167bc33
Chloé <cadonon.dev@gmail.com>
2023-04-17 16:07:57 +0200

    added styles

--- src/components/ui/FilterTag.vue
+++ src/components/ui/FilterTag.vue
@@ -2,13 +2,12 @@
 </script>
 
 <template>
-    <button class="filter" :id="filter.url" @click="filterProjects">
+    <button class="filter-btn" :id="filter.url" @click="filterProjects">
         <slot></slot>
     </button>
 </template>
 
 <script>
-
 export default {
     name: "FilterTag",
     emits: ['currentFilter'],
@@ -20,9 +19,31 @@ export default {
     },
     methods: {
         filterProjects() {
-            this.$emit('currentFilter', this.filter.url);
+            this.$emit('currentFilter', this.filter.fields.url);
         }
     }
 }
-
 </script>
+
+<style scoped lang="scss">
+@use '../../assets/variables.scss' as v;
+
+button.filter-btn {
+    display: inline-block;
+    padding: 4px 24px;
+    border-radius: 30px;
+    background-color: v.$secondary-050;
+    font-size: v.$font-body;
+    font-weight: v.$font-medium;
+    color: v.$primary-700;
+    text-align: center;
+    border: 2px solid v.$primary-700;
+    cursor: pointer;
+
+    &:hover {
+        background-color: v.$primary-700;
+        color: v.$secondary-050;
+        border: 2px solid v.$primary-700;
+    }
+}
+</style>
