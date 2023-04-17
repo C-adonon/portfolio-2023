01b63dd8bcde7fd2f8f0718f74704d117968259f
Chloé <cadonon.dev@gmail.com>
2023-04-17 16:36:54 +0200

    initialized top title component

---
+++ src/components/ui/TopTitle.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="top-title">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TopTitle",
+}
+</script>
+
+<style scoped lang="scss">
+@use '../../assets/variables.scss' as v;
+
+div.top-title {
+    padding:  4% 8%;
+    text-align: center;
+    font-size: 100px;
+    font-weight: v.$font-bold;
+    color: v.$neutral-800;
+}
+</style>
