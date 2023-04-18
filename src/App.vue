b1e2879128209a07e364bb8dec35e3a8a161cfb0
Chloé <cadonon.dev@gmail.com>
2023-04-18 10:08:16 +0200

    removed main tag

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,6 @@ import Footer from './components/navigation/Footer.vue';
 
 <template>
   <Navbar />
-  <main>
     <RouterView />
-  </main>
   <Footer />
 </template>
-
-<style scoped lang="scss">
-main {
-  width: 80vw;
-  margin: 0 auto;
-  margin-top: calc(80px + 10%);
-}
-</style>
