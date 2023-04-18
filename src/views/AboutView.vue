f9e402a24c0565077ebfc6cada5ada3245954a91
Chloé <cadonon.dev@gmail.com>
2023-04-18 10:11:22 +0200

    initialized contact component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,55 @@
+<script setup>
+import TopTitle from '../components/ui/TopTitle.vue';
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main class="about">
+    <TopTitle>A propos de moi</TopTitle>
+    <section class="about-content">
+      <div class="about-content-text">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum provident soluta, ea nisi officia, laborum animi
+          minima praesentium quo exercitationem consectetur odit. Autem aut fugiat dolore cumque doloribus eius explicabo?
+        </p>
+      </div>
+      <div class="about-image">
+        <img src="" alt="moi">
+      </div>
+    </section>
+  </main>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+<style scoped lang="scss">
+@use '../assets/variables.scss' as v;
+
+section.about-content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .about-image {
+    width: 40%;
+    // height: 50%;
+    img {
+      width: 100%;
+      height: 100%;
+    }
   }
-}
-</style>
+
+  .about-content-text {
+    width: 40%;
+
+    h2 {
+      font-size: 50px;
+      font-weight: v.$font-bold;
+      margin-bottom: 2%;
+      color: v.$neutral-900;
+    }
+
+    p {
+      font-size: v.$font-body;
+      font-weight: v.$font-regular;
+      text-align: justify;
+      margin-bottom: 6%;
+    }
+  }
+}</style>
