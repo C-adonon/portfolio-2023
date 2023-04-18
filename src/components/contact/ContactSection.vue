ab9504e84697b225206532078de078cecf168866
Chloé <cadonon.dev@gmail.com>
2023-04-18 10:10:06 +0200

    initialized contact section

---
+++ src/components/contact/ContactSection.vue
@@ -0,0 +1,40 @@
+<script setup>
+import PrimaryBtn from '../ui/PrimaryBtn.vue';
+</script>
+<template>
+    <div class="contact">
+        <h3>Restons en contact</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quaerat quae eum aperiam. Dicta velit
+            excepturi pariatur ducimus ex rerum soluta culpa itaque architecto! Ea veritatis a quod repellendus blanditiis?
+        </p>
+        <PrimaryBtn email="true">Contactez-moi</PrimaryBtn>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use '../../assets/variables.scss' as v;
+
+div.contact {
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 56px 48px;
+    cursor: pointer;
+    margin: auto;
+    
+
+    h3 {
+        font-size: 50px;
+        font-weight: v.$font-bold;
+        margin-bottom: 2%;
+        color: v.$neutral-900;
+    }
+    p {
+        font-size: v.$font-body;
+        font-weight: v.$font-regular;
+        text-align: center;
+        margin-bottom: 6%;
+    }
+}
+</style>
