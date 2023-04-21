82065e62214db21c0c4811e5b13c086c81afd41b
Chloé <cadonon.dev@gmail.com>
2023-04-21 11:49:34 +0200

    initialized news ticker

--- src/components/ui/NewsTicker.vue
+++ src/components/ui/NewsTicker.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>
-        NewsTicker
+    <div class="new-ticker">
+        <slot></slot>
     </div>
 </template>
+
+<style scoped lang="scss">
+@use '../../assets/variables.scss' as v;
+</style>
