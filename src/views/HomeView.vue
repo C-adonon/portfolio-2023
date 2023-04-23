39c2438cbd2550742c5c56f82cfa9b987c50adb5
Chloé <cadonon.dev@gmail.com>
2023-04-23 22:26:00 +0200

    modified styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-// import NewsTicker from '../components/ui/NewsTicker.vue';
+import NewsTicker from '../components/ui/NewsTicker.vue';
 import ProjectCard from '../components/card/ProjectCard.vue';
 import ContactSection from '../components/contact/ContactSection.vue';
 import PrimaryBtn from '../components/ui/PrimaryBtn.vue';
 import CardSuggestion from '../components/card/CardSuggestion.vue';
+import RadialGrandient from '../components/gradient/RadialGrandient.vue';
 const assetUrl = import.meta.env.VITE_ASSET_URL;
 </script>
 
@@ -11,22 +12,22 @@ const assetUrl = import.meta.env.VITE_ASSET_URL;
 <template>
   <main>
     <!-- Hero/Intro -->
-    <div class="gradient-background"></div>
-
     <section class="intro">
       <div class="intro-info">
         <h1>Chloe Adonon</h1>
-        <h2> <span>Creative</span> web Developper</h2>
+        <h2> <span>Creative</span> web developer</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, animi molestias quos, tempore adipisci aliquid
-          hic quidem magni laboriosam quasi a !</p>
+          hic !</p>
         <PrimaryBtn :cv="true">Télécharger mon CV</PrimaryBtn>
       </div>
       <div class="intro-picture">
         <img :src='assetUrl + "inoya-ads-3.jpg"' alt="chloe-adonon">
+        <!-- <RadialGrandient /> -->
       </div>
     </section>
 
     <!-- About -->
+    <NewsTicker>A propos</NewsTicker>
     <section class="about">
       <div class="about-picture">
         <img :src='assetUrl + "inoya-ads-3.jpg"' alt="chloe-adonon">
@@ -41,18 +42,20 @@ const assetUrl = import.meta.env.VITE_ASSET_URL;
           aliquid hic quidem magni laboriosam quasi a, quae facere accusamus reprehenderit. Maxime nostrum voluptas
           provident
           soluta!</p>
-        <PrimaryBtn url="/about">A propos de moi</PrimaryBtn>
+        <PrimaryBtn route="/about">A propos de moi</PrimaryBtn>
       </div>
     </section>
 
-
     <!-- Projects -->
+    <NewsTicker>Mes projets</NewsTicker>
     <section class="projects">
       <CardSuggestion />
     </section>
 
   </main>
+
   <!-- Contact -->
+  <NewsTicker>Mes projets</NewsTicker>
   <section class="contact">
     <ContactSection />
   </section>
@@ -63,10 +66,12 @@ const assetUrl = import.meta.env.VITE_ASSET_URL;
 export default {
   name: "HomeView",
   components: {
-    // NewsTicker,
+    NewsTicker,
     PrimaryBtn,
     ProjectCard,
-    ContactSection
+    ContactSection,
+    CardSuggestion,
+    RadialGrandient
   }
 }
 
@@ -75,99 +80,133 @@ export default {
 <style scoped lang="scss">
 @use '../assets/variables.scss' as v;
 
-$overlap: -8%;
+$overlap: -5%;
 
 section {
-  margin: 10% 0;
+  width: 80%;
+  margin: 2% auto 8% auto;
 }
 
-section.intro {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+main {
+  width: 100%;
+  margin: 0 auto;
+  margin-top: calc(80px + 5%);
 
-  // div.intro-info,
-  // div.intro-picture {
-  //   width: 50%;
-  // }
+  section.intro {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  div.intro-info {
-    margin-right: $overlap;
-    width: 60%;
+    div.intro-info {
+      margin-right: $overlap;
+      width: 65%;
 
-    h1 {
-      font-size: v.$font-title-L;
-      font-weight: v.$font-bold;
-      color: v.$neutral-900;
-    }
+      h1 {
+        font-size: 50px;
+        font-weight: v.$font-semi-bold;
+        color: v.$neutral-900;
+      }
 
-    h2 {
-      font-size: v.$font-title-XL;
-      font-weight: v.$font-bold;
-      color: v.$primary-700;
-      line-height: 95px;
+      h2 {
+        font-size: v.$font-title-XXL;
+        font-weight: v.$font-bold;
+        color: v.$primary-700;
+        line-height: 110px;
 
-      span {
-        font-family: v.$font-family-elastic;
+        span {
+          font-family: v.$font-family-elastic;
+          color: v.$primary-900;
+        }
+      }
+
+      p {
+        font-size: v.$font-header2;
+        font-weight: v.$font-regular;
+        margin-bottom: 4%;
+        max-width: 70%;
+        margin-top: 16px;
       }
     }
 
-    p {
-      font-size: v.$font-header2;
-      font-weight: v.$font-regular;
-      margin-bottom: 4%;
-      max-width: 70%;
+    div.intro-picture {
+      margin-left: $overlap;
+      z-index: -1;
+      width: 40%;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+
+      // div.gradient {
+      //   position: absolute;
+      //   top: 30%;
+      //   left: 70%;
+      //   width: 400px;
+      //   height: 400px;
+      //   z-index: -1;
+      //   border-radius: 50%;
+      //   filter: blur(75px);
+      //   background: linear-gradient(50deg, #d3c0f7, #2c36ef);
+      //   background-size: 400% 400%;
+      //   animation: AnimationName 4s ease infinite;
+      // }
+
+      // @keyframes AnimationName {
+      //   0% {
+      //     background-position: 87% 0%
+      //   }
+
+      //   50% {
+      //     background-position: 14% 100%
+      //   }
+
+      //   100% {
+      //     background-position: 87% 0%
+      //   }
+      // }
+
     }
   }
 
-  div.intro-picture {
-    margin-left: $overlap;
-    z-index: -1;
-    width: 40%;
+  section.about {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 32px;
 
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-  }
-}
-
-section.about {
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-
-  div.about-picture,
-  div.about-info {
-    width: 40%;
-  }
-
-  div.about-picture {
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-    }
-  }
-
-  div.about-info {
-    h2 {
-      font-size: v.$font-header2;
-      font-weight: v.$font-bold;
-      color: v.$neutral-900;
+    div.about-picture,
+    div.about-info {
+      width: 40%;
     }
 
-    h3 {
-      font-size: v.$font-header3;
-      font-weight: v.$font-light;
-      color: v.$neutral-700;
+    div.about-picture {
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
     }
 
-    p {
-      font-size: v.$font-body;
-      font-weight: v.$font-regular;
-      margin-bottom: 4%;
+    div.about-info {
+      h2 {
+        font-size: v.$font-header2;
+        font-weight: v.$font-bold;
+        color: v.$neutral-900;
+      }
+
+      h3 {
+        font-size: v.$font-header3;
+        font-weight: v.$font-light;
+        color: v.$neutral-700;
+      }
+
+      p {
+        font-size: v.$font-body;
+        font-weight: v.$font-regular;
+        margin-bottom: 4%;
+      }
     }
   }
 }
@@ -175,7 +214,6 @@ section.about {
 section.contact {
   width: 100vw;
   height: 90vh;
-  margin: 10% auto;
   background-image: url("../contact-bg.svg");
   background-size: cover;
   background-position: center;
