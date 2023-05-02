a196aeda898c2fa25d15475530fb0f2274ae4b7d
Chloé <cadonon.dev@gmail.com>
2023-05-02 23:10:44 +0200

    added responsive styles

--- src/components/navigation/Navbar.vue
+++ src/components/navigation/Navbar.vue
@@ -7,7 +7,10 @@ import { RouterLink, RouterView } from 'vue-router'
         <div id="navbar-logo">
             <RouterLink to="/"><img src="/logo6.svg" alt="logo" title="logo"></RouterLink>
         </div>
-        <div class="links-container">
+        <div class="navbar-mobile-open" @click="openMobileMenu">
+            <img class="menu-icon" src="/menu-icon.svg" alt="menu-icon" title="menu-icon">
+        </div>
+        <div id="all-links" class="links-container">
             <ul class="navbar-list">
                 <li class="navbar-links">
                     <RouterLink to="/about">A propos</RouterLink>
@@ -24,6 +27,30 @@ import { RouterLink, RouterView } from 'vue-router'
         </div>
     </nav>
 </template>
+
+<script>
+export default {
+    name: 'Navbar',
+    methods: {
+        openMobileMenu() {
+            if (window.innerWidth < 900) {
+                const linksContainer = document.querySelector('#all-links');
+                linksContainer.classList.toggle('mobile-links-container');
+                linksContainer.classList.toggle('links-container');
+                let menuIcon = document.querySelector('.menu-icon');
+
+                if (menuIcon.src.includes('menu-icon.svg')) {
+                    menuIcon.src = '/close-icon.svg';
+                } else {
+                    menuIcon.src = '/menu-icon.svg';
+                }
+
+            }
+
+        }
+    }
+}
+</script>
 
 <style scoped lang="scss">
 @use '../../assets/variables.scss' as v;
@@ -56,6 +83,17 @@ nav#navbar {
                 height: 60px;
                 width: 60px;
             }
+        }
+    }
+
+    div.navbar-mobile-open {
+        display: none;
+        margin-right: 2%;
+        cursor: pointer;
+
+        img {
+            height: 40px;
+            width: 40px;
         }
     }
 
@@ -110,6 +148,7 @@ nav#navbar {
             text-align: center;
             cursor: pointer;
             padding: 16px 24px;
+
             a {
                 text-decoration: none;
                 color: v.$secondary-050;
@@ -126,5 +165,115 @@ nav#navbar {
 
     }
 
+}
+
+// Desktop
+@media (min-width: 901px) and (max-width: 1023px) {
+
+    nav#navbar {
+
+        div.links-container {
+            width: 70%;
+        }
+    }
+
+}
+
+// Tablet & mobile
+@media (max-width: 900px) {
+    nav#navbar {
+        height: 56px;
+        margin-bottom: calc(56px + 4px);
+
+        #navbar-logo {
+
+            a {
+
+                img {
+                    height: 46px;
+                    width: 46px;
+                }
+            }
+        }
+
+
+        div.navbar-mobile-open {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            margin-right: 2%;
+            cursor: pointer;
+
+            img {
+                height: 36px;
+                width: 36px;
+            }
+        }
+
+        div.links-container {
+            display: none;
+        }
+
+        .mobile-links-container {
+            position: absolute;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            align-items: center;
+            // padding: 64px 0px 0px;
+            // gap: 111px;
+            background: v.$neutral-900;
+            top: 56px;
+            left: 0;
+            width: 100%;
+            height: 80vh;
+
+            ul.navbar-list {
+                list-style: none;
+                padding: 0;
+                width: 100%;
+
+                li.navbar-links {
+                    a {
+                        font-size: v.$font-header2;
+                        font-weight: v.$font-semi-bold;
+                        color: v.$secondary-050;
+                        text-decoration: none;
+                        text-align: center;
+                        display: block;
+                        padding: 36px 0px;
+                        width: 100%;
+                        height: 100%;
+
+                    }
+                }
+
+            }
+        }
+
+        div.contact {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            font-size: v.$font-header2;
+            font-weight: v.$font-bold;
+            background-color: v.$neutral-900;
+            text-align: center;
+            padding: 24px 0px;
+            border-width: 2px;
+            border-style: solid;
+            border-image: linear-gradient(to right, v.$primary-050 , v.$primary-700) 1;
+
+
+            a {
+                text-decoration: none;
+                color: v.$secondary-050;
+            }
+
+        }
+
+    }
 }
 </style>
